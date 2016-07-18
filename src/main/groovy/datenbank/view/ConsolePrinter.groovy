a7c9dba4a9fdfe96a0c38da6fa07bc6f4caffa72
Kasper Hansen <kasper.hansen.denmark@gmail.com>
2016-07-18 13:13:15 +0200

    summary action to view

--- src/main/groovy/datenbank/view/ConsolePrinter.groovy
+++ src/main/groovy/datenbank/view/ConsolePrinter.groovy
@@ -19,11 +19,13 @@ public class ConsolePrinter implements Observer {
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		log.info('update gui')
+		log.info("update gui $arg0")
 		if(arg0 instanceof TestResult) {		
 			print "\rCompare ${arg0.compared} of  ${arg0.total} (skipped: $arg0.skipped), elapsed: $arg0.elapsed seconds"
 		} else if(arg0 instanceof QueryResult) {		
 			print "\rExecuting ${arg0.i} of  ${arg0.total}, elapsed: $arg0.elapsed seconds"
+		} else if(arg0 instanceof Summary) {		
+			println ""
 		} else {
 			log.debug("missing action to update ui")
 			
