b14a6ca595b5d2a2b76fcc50b5eab37ac346357b
Kasper Hansen <kasper.hansen.denmark@gmail.com>
2016-08-02 12:54:40 +0200

    Update FxPrinter.groovy

--- src/main/groovy/datenbank/view/FxPrinter.groovy
+++ src/main/groovy/datenbank/view/FxPrinter.groovy
@@ -5,11 +5,9 @@ import datenbank.engine.Init
 import datenbank.engine.ResultTester
 import datenbank.model.Summary
 import datenbank.model.TestCase
+import datenbank.model.Variables
 
 import java.util.Observer;
-
-
-
 
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -21,6 +19,7 @@ import javafx.scene.control.ContextMenu
 import javafx.scene.control.Control
 import javafx.scene.control.MenuItem
 import javafx.scene.control.Callback
+import javafx.scene.control.Alert
 import javafx.scene.control.TableCell
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow
@@ -32,63 +31,65 @@ import javafx.scene.layout.HBox
 import javafx.scene.layout.VBox;
 import javafx.util.Callback
 import javafx.scene.paint.Color;
+
+
+
 class FxPrinter extends Application implements Observer {
-	
+
 	def tv
 	def compare, exec
 	def menu
-	def itemExec, itemComp
+	def itemExec, itemComp, itemOpenTgt, itemOpenSrc
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
-		tv = new TableView()
-		def box = new VBox()		
-				
-        def colFile = new TableColumn("Name")
 
+		tv = new TableView()
+		def box = new VBox()
+
+		def colFile = new TableColumn("Name")
 		def colSkipped = new TableColumn("Skipped")
 		def colError = new TableColumn("Status")
 		def colResultFlag = new TableColumn("Result flag")
 		def colElapsed = new TableColumn("Elapsed")
 		def colElapsedTest = new TableColumn("Elapsed compare")
-		
-		colError.setCellFactory(new Callback<TableColumn<TestCase, Integer>, TableCell<TestCase, Integer>>() {
-			@Override
-			public TableCell<TestCase, Integer> call(TableColumn<TestCase, Integer> param) {
-				return new TableCell<TestCase, Integer>() {
 
+		colError.setCellFactory(new Callback<TableColumn<TestCase, Integer>, TableCell<TestCase, Integer>>() {
 					@Override
-					protected void updateItem(Integer item, boolean empty) {
-						super.updateItem(item, empty);
-						if(item == -1) {
-							setTextFill(Color.BLACK);
-							setText("")
-						}
-						if(item == 1) {
-							setTextFill(Color.RED);
-							setText("FAILURE Execute")
-						}
-						if(item == 2) {
-							setTextFill(Color.RED);
-							setText("FAILURE Compare")
-						}
-						
-						if(item == 3) { 
-							setTextFill(Color.GREEN);
-							setText("SUCCESS Execute")
-						}
-						
-						if(item == 4) {
-							setTextFill(Color.GREEN);
-							setText("SUCCESS Compare")
-						}
+					public TableCell<TestCase, Integer> call(TableColumn<TestCase, Integer> param) {
+						return new TableCell<TestCase, Integer>() {
+
+									@Override
+									protected void updateItem(Integer item, boolean empty) {
+										super.updateItem(item, empty);
+										if(item == -1) {
+											setTextFill(Color.BLACK);
+											setText("")
+										}
+										if(item == 1) {
+											setTextFill(Color.RED);
+											setText("FAILURE Execute")
+										}
+										if(item == 2) {
+											setTextFill(Color.RED);
+											setText("FAILURE Compare")
+										}
+
+										if(item == 3) {
+											setTextFill(Color.GREEN);
+											setText("SUCCESS Execute")
+										}
+
+										if(item == 4) {
+											setTextFill(Color.GREEN);
+											setText("SUCCESS Compare")
+										}
+									}
+								};
 					}
-				};
-			}
-		});
-		
-			
-        colFile.setCellValueFactory(new PropertyValueFactory("name"))
+				});
+
+
+		colFile.setCellValueFactory(new PropertyValueFactory("name"))
 		colSkipped.setCellValueFactory(new PropertyValueFactory("skipped"))
 		colError.setCellValueFactory(new PropertyValueFactory("errors"))
 		colResultFlag.setCellValueFactory(new PropertyValueFactory("resultFlag"))
@@ -96,134 +97,184 @@ class FxPrinter extends Application implements Observer {
 		colElapsedTest.setCellValueFactory(new PropertyValueFactory("elapsedTest"))
 		def init = new Init(ui: this)
 		def summary = init.init()
-		
+
 		colFile.width = 200
+		colError.width = 200
 		colElapsedTest.width = 150
 		tv.getColumns().addAll(colFile, colSkipped, colError, colResultFlag, colElapsed, colElapsedTest)
 
 		def rt = new ResultTester()
 		def ex = new Executor()
-        compare = new Button("Compare All");
+		compare = new Button("Compare All");
 		compare.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				
-				Thread.start {
+					@Override
+					public void handle(ActionEvent event) {
 
-					btnUpdate(true)
-					rt.runAll(summary)
-					btnUpdate(false)
-				}
-			}
-		});
-	
+						Thread.start {
+
+							btnUpdate(true)
+							rt.runAll(summary)
+							btnUpdate(false)
+						}
+					}
+				});
+
 		exec = new Button("Execute All");
 		exec.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				
-				Thread.start {
+					@Override
+					public void handle(ActionEvent event) {
 
-					btnUpdate(true)
-					ex.runAll(summary)
-					btnUpdate(false)
-				}
-				
-			}
-		});
-	
-	
+						Thread.start {
+
+							btnUpdate(true)
+							ex.runAll(summary)
+							btnUpdate(false)
+						}
+					}
+				});
+
+
 		menu = new ContextMenu();
 		itemExec = new MenuItem("Execute");
 		itemComp = new MenuItem("Compare");
+		itemOpenTgt = new MenuItem("Open target file");
+		itemOpenSrc = new MenuItem("Open source file");
 		menu.getItems().add(itemExec);
 		menu.getItems().add(itemComp);
+		menu.getItems().add(itemOpenTgt);
+		menu.getItems().add(itemOpenSrc);
 		tv.setContextMenu(menu);
-        
-		itemExec.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				def testCase = (TestCase) tv.getSelectionModel().getSelectedItem();
-				Thread.start {
-					
-					btnUpdate(true)
-					ex.runOne(testCase)
-					btnUpdate(false)
-				}
-				
-			}
-	   });
 
-	   itemComp.setOnAction(new EventHandler<ActionEvent>() {
-		   @Override
-		   public void handle(ActionEvent event) {
-			   def testCase = (TestCase) tv.getSelectionModel().getSelectedItem();
-			   Thread.start {
-				   
-				   btnUpdate(true)
-				   rt.runOne(testCase)
-				   btnUpdate(false)
-			   }
-			   
-		   }
-	  });
-		
-		
+		itemExec.setOnAction(new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						def testCase = (TestCase) tv.getSelectionModel().getSelectedItem();
+						Thread.start {
+
+							btnUpdate(true)
+							ex.runOne(testCase)
+							btnUpdate(false)
+						}
+					}
+				});
+
+		itemComp.setOnAction(new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						def testCase = (TestCase) tv.getSelectionModel().getSelectedItem();
+						Thread.start {
+
+							btnUpdate(true)
+							rt.runOne(testCase)
+							btnUpdate(false)
+						}
+					}
+				});
+
+
+
+
+		itemOpenTgt.setOnAction(new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						def testCase = (TestCase) tv.getSelectionModel().getSelectedItem();
+
+						btnUpdate(true)
+
+						Thread.start {
+
+							btnUpdate(true)
+							try {
+								"notepad ${Variables.path}Target/${testCase.name}.sql".execute()
+							} catch(all) {
+								alert("Open file error", "Couldn't open file. Please check that it exists!")
+							}
+
+							btnUpdate(false)
+						}
+					}
+				});
+
+
+		itemOpenSrc.setOnAction(new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						def testCase = (TestCase) tv.getSelectionModel().getSelectedItem();
+
+						btnUpdate(true)
+
+						Thread.start {
+
+							btnUpdate(true)
+							try {
+								"notepad ${Variables.path}Source/${testCase.name}.sql".execute()
+							} catch(all) {
+								alert()
+							}
+
+							btnUpdate(false)
+						}
+					}
+				});
+
 		def hbox = new HBox()
 		hbox.getChildren().addAll(exec, compare)
-        box.getChildren().addAll(tv, hbox);
-        primaryStage.setScene(new Scene(box,800,400));
+		box.getChildren().addAll(tv, hbox);
+		primaryStage.setScene(new Scene(box,800,400));
 		primaryStage.setResizable(false)
 		primaryStage.setTitle("DW Test Toolkit")
-        primaryStage.show();
-		
+		primaryStage.show();
 	}
-	
+
 	def btnUpdate(bool) {
 		Platform.runLater(new Runnable() {
-			@Override public void run() {
-				itemComp.setDisable(bool)
-				itemExec.setDisable(bool)
-				exec.setDisable(bool)
-				compare.setDisable(bool)
-			}
-		});
-		
+					@Override public void run() {
+						itemComp.setDisable(bool)
+						itemExec.setDisable(bool)
+						itemOpenSrc.setDisable(bool)
+						itemOpenTgt.setDisable(bool)
+						exec.setDisable(bool)
+						compare.setDisable(bool)
+					}
+				});
+	}
+
+
+	def alert(header, msg) {
+		Platform.runLater(new Runnable() {
+					@Override public void run() {
+						def alert = new Alert(Alert.AlertType.ERROR);
+						alert.setTitle("Error Dialog");
+						alert.setHeaderText(header);
+						alert.setContentText(msg);
+						alert.showAndWait();
+					}
+				})
 	}
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
 
-		if(arg0 instanceof Summary) {			
+		if(arg0 instanceof Summary) {
 			Platform.runLater(new Runnable() {
-				@Override public void run() {
-					tv?.getItems().removeAll(arg0.testCases)
-					tv?.setItems(FXCollections.observableArrayList(arg0.testCases))
-				}
-			});
-			
-			
+						@Override public void run() {
+							tv?.getItems().removeAll(arg0.testCases)
+							tv?.setItems(FXCollections.observableArrayList(arg0.testCases))
+						}
+					});
 		}
-		
+
 		if(arg0 instanceof TestCase) {
 			Platform.runLater(new Runnable() {
-				@Override public void run() {					
-					
-					def items = []
-					tv?.getItems().each {testCase ->
-						
-						items << testCase
-					}
-					
-					tv?.getItems().removeAll(items)
-					tv?.setItems(FXCollections.observableArrayList(items))
-				}
-			});
-			
-			
+						@Override public void run() {
+
+							def items = []
+							tv?.getItems().each {testCase -> items << testCase }
+
+							tv?.getItems().removeAll(items)
+							tv?.setItems(FXCollections.observableArrayList(items))
+						}
+					});
 		}
-
-
 	}
-
 }
