c9905e706f88f8582c1d4de4fae228fefd7bcc87
datenbank <kasper.hansen.denmark@gmail.com>
2016-08-21 06:54:18 +0200

    oracle meta data query

--- src/main/groovy/datenbank/model/Model.groovy
+++ src/main/groovy/datenbank/model/Model.groovy
@@ -49,30 +49,29 @@ class Model {
 	def writeModelToFile() {
 		def file = new File("${Variables.path}${Variables.model}")
 		file.write("")
-		
+
 		tables.each { t ->
-			t.columns.each { c -> 
+			t.columns.each { c ->
 				if(c.columnRef)
-					file << "${t.database}${Variables.csvSeperator}${t.schema}${Variables.csvSeperator}${t.table}${Variables.csvSeperator}${c.column}${Variables.csvSeperator}${c.ordinal}${Variables.csvSeperator}YES${Variables.csvSeperator}${c.dataType}${Variables.csvSeperator}${c.isPrimaryKey}${Variables.csvSeperator}${c.columnRef.tableRef.database}${Variables.csvSeperator}${c.columnRef.tableRef.schema}${Variables.csvSeperator}${c.columnRef.tableRef.table}${Variables.csvSeperator}${c.columnRef.column}${Variables.csvSeperator}${c.columnRef.ordinal}${Variables.csvSeperator}YES${Variables.csvSeperator}${c.columnRef.dataType}${Variables.csvSeperator}${c.columnRef.isPrimaryKey}${Variables.csvSeperator}${c.testType}${Variables.csvSeperator}${t.where}${Variables.csvSeperator}${c.columnRef.tableRef.where}\r\n" }
+					file << "${t.database}${Variables.csvSeperator}${t.schema}${Variables.csvSeperator}${t.table}${Variables.csvSeperator}${c.column}${Variables.csvSeperator}${c.ordinal}${Variables.csvSeperator}YES${Variables.csvSeperator}${c.dataType}${Variables.csvSeperator}${c.isPrimaryKey}${Variables.csvSeperator}${c.columnRef.tableRef.database}${Variables.csvSeperator}${c.columnRef.tableRef.schema}${Variables.csvSeperator}${c.columnRef.tableRef.table}${Variables.csvSeperator}${c.columnRef.column}${Variables.csvSeperator}${c.columnRef.ordinal}${Variables.csvSeperator}YES${Variables.csvSeperator}${c.columnRef.dataType}${Variables.csvSeperator}${c.columnRef.isPrimaryKey}${Variables.csvSeperator}${c.testType}${Variables.csvSeperator}${t.where}${Variables.csvSeperator}${c.columnRef.tableRef.where}\r\n"
+			}
 		}
 	}
-	
+
 	def loadModelFromDb(group) {
-		
-		def sql 
-		def sqlSrc 
-		
+
+		def sql
+		def sqlSrc
+		def srcQuery
+		def tgtQuery
 		if(group == "Default") {
 			sql = Sql.newInstance(Variables.targetConnection, Variables.targetDriver )
 			sqlSrc = Sql.newInstance(Variables.sourceConnection, Variables.sourceDriver )
-			
-			
-		} else {
-			sql = Sql.newInstance(Variables.config.groups."${group}".target, Variables.config.groups."${group}".targetDriver)
-			sqlSrc = Sql.newInstance(Variables.config.groups."${group}".source, Variables.config.groups."${group}".sourceDriver)
-		
-		}
-		sqlSrc.eachRow('''
+
+			if(Variables.targetDriver.contains("Oracle")) {
+				tgtQuery = "select '-', '-', table_name, column_name, 1, nullable, data_type, 0 from cols"
+			} else {
+				tgtQuery = '''
 			SELECT TABLE_CATALOG,
 			       TABLE_SCHEMA,
 			       TABLE_NAME,
@@ -82,61 +81,126 @@ class Model {
 			       DATA_TYPE,
 			       
 			                (SELECT MAX('1')
-			                 FROM [INFORMATION_SCHEMA].[KEY_COLUMN_USAGE] kcu
-								INNER JOIN [INFORMATION_SCHEMA].[TABLE_CONSTRAINTS] tc 
+			                 FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu
+								INNER JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc 
 									ON kcu.CONSTRAINT_NAME=tc.CONSTRAINT_NAME and tc.CONSTRAINT_TYPE='PRIMARY KEY'
-									AND tc.[TABLE_CATALOG]=c.TABLE_CATALOG
-									AND tc.[TABLE_SCHEMA]=c.[TABLE_SCHEMA]
-									AND tc.[TABLE_NAME]=c.[TABLE_NAME]
+									AND tc.TABLE_CATALOG=c.TABLE_CATALOG
+									AND tc.TABLE_SCHEMA=c.TABLE_SCHEMA
+									AND tc.TABLE_NAME=c.TABLE_NAME
 			                 WHERE kcu.TABLE_CATALOG=c.TABLE_CATALOG
 			                   AND kcu.TABLE_SCHEMA=c.TABLE_SCHEMA
 			                   AND kcu.TABLE_NAME=c.TABLE_NAME
 			                   AND kcu.COLUMN_NAME=c.COLUMN_NAME) IS_PK
-			FROM [INFORMATION_SCHEMA].[COLUMNS] c
-			order by 1,2,3,5
-			''') { row ->
-			
+			FROM INFORMATION_SCHEMA.COLUMNS c
+			'''
+			}
+
+			if(Variables.sourceDriver.contains("Oracle")) {
+				srcQuery = "select '-', '-', table_name, column_name, 1, nullable, data_type, '0' from cols"
+			} else {
+				srcQuery = '''
+			SELECT TABLE_CATALOG,
+			       TABLE_SCHEMA,
+			       TABLE_NAME,
+			       COLUMN_NAME,
+			       ORDINAL_POSITION,
+			       IS_NULLABLE,
+			       DATA_TYPE,
+			       
+			                (SELECT MAX('1')
+			                 FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu
+								INNER JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc 
+									ON kcu.CONSTRAINT_NAME=tc.CONSTRAINT_NAME and tc.CONSTRAINT_TYPE='PRIMARY KEY'
+									AND tc.TABLE_CATALOG=c.TABLE_CATALOG
+									AND tc.TABLE_SCHEMA=c.TABLE_SCHEMA
+									AND tc.TABLE_NAME=c.TABLE_NAME
+			                 WHERE kcu.TABLE_CATALOG=c.TABLE_CATALOG
+			                   AND kcu.TABLE_SCHEMA=c.TABLE_SCHEMA
+			                   AND kcu.TABLE_NAME=c.TABLE_NAME
+			                   AND kcu.COLUMN_NAME=c.COLUMN_NAME) IS_PK
+			FROM INFORMATION_SCHEMA.COLUMNS c
+			'''
+			}
+		} else {
+			sql = Sql.newInstance(Variables.config.groups."${group}".target, Variables.config.groups."${group}".targetDriver)
+			sqlSrc = Sql.newInstance(Variables.config.groups."${group}".source, Variables.config.groups."${group}".sourceDriver)
+
+			if(Variables.config.groups."${group}".targetDriver.contains("Oracle")) {
+				tgtQuery = "select '-', '-', table_name, column_name, 1, nullable, data_type, '0' from cols"
+			} else {
+				tgtQuery = '''
+			SELECT TABLE_CATALOG,
+			       TABLE_SCHEMA,
+			       TABLE_NAME,
+			       COLUMN_NAME,
+			       ORDINAL_POSITION,
+			       IS_NULLABLE,
+			       DATA_TYPE,
+			       
+			                (SELECT MAX('1')
+			                 FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu
+								INNER JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc 
+									ON kcu.CONSTRAINT_NAME=tc.CONSTRAINT_NAME and tc.CONSTRAINT_TYPE='PRIMARY KEY'
+									AND tc.TABLE_CATALOG=c.TABLE_CATALOG
+									AND tc.TABLE_SCHEMA=c.TABLE_SCHEMA
+									AND tc.TABLE_NAME=c.TABLE_NAME
+			                 WHERE kcu.TABLE_CATALOG=c.TABLE_CATALOG
+			                   AND kcu.TABLE_SCHEMA=c.TABLE_SCHEMA
+			                   AND kcu.TABLE_NAME=c.TABLE_NAME
+			                   AND kcu.COLUMN_NAME=c.COLUMN_NAME) IS_PK
+			FROM INFORMATION_SCHEMA.COLUMNS c
+			'''
+			}
+
+			if(Variables.config.groups."${group}".sourceDriver.contains("Oracle")) {
+				srcQuery = "select '-', '-', table_name, column_name, 1, nullable, data_type, '0' from cols"
+			} else {
+				srcQuery = '''
+			SELECT TABLE_CATALOG,
+			       TABLE_SCHEMA,
+			       TABLE_NAME,
+			       COLUMN_NAME,
+			       ORDINAL_POSITION,
+			       IS_NULLABLE,
+			       DATA_TYPE,
+			       
+			                (SELECT MAX('1')
+			                 FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu
+								INNER JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc 
+									ON kcu.CONSTRAINT_NAME=tc.CONSTRAINT_NAME and tc.CONSTRAINT_TYPE='PRIMARY KEY'
+									AND tc.TABLE_CATALOG=c.TABLE_CATALOG
+									AND tc.TABLE_SCHEMA=c.TABLE_SCHEMA
+									AND tc.TABLE_NAME=c.TABLE_NAME
+			                 WHERE kcu.TABLE_CATALOG=c.TABLE_CATALOG
+			                   AND kcu.TABLE_SCHEMA=c.TABLE_SCHEMA
+			                   AND kcu.TABLE_NAME=c.TABLE_NAME
+			                   AND kcu.COLUMN_NAME=c.COLUMN_NAME) IS_PK
+			FROM INFORMATION_SCHEMA.COLUMNS c
+			'''
+			}
+		}
+
+
+
+		sqlSrc.eachRow(srcQuery) { row ->
+
 			def st = new Table(table: row[2], schema: row[1], database: row[0], where: "-")
 			addSrc st
 
 			st = getSrc(st.table, st.schema, st.database)
 
 			st.addColumn(new Column(column: row[3], dataType: row[6], isPrimaryKey: row[7] == null ? 0 : 1, tableRef: st, ordinal: row[4]))
+		}
+		sql.eachRow(tgtQuery) { row ->
 
-			}
-			sql.eachRow('''
-			SELECT TABLE_CATALOG,
-			       TABLE_SCHEMA,
-			       TABLE_NAME,
-			       COLUMN_NAME,
-			       ORDINAL_POSITION,
-			       IS_NULLABLE,
-			       DATA_TYPE,
-			       
-			                (SELECT MAX('1')
-			                 FROM [INFORMATION_SCHEMA].[KEY_COLUMN_USAGE] kcu
-								INNER JOIN [INFORMATION_SCHEMA].[TABLE_CONSTRAINTS] tc 
-									ON kcu.CONSTRAINT_NAME=tc.CONSTRAINT_NAME and tc.CONSTRAINT_TYPE='PRIMARY KEY'
-									AND tc.[TABLE_CATALOG]=c.TABLE_CATALOG
-									AND tc.[TABLE_SCHEMA]=c.[TABLE_SCHEMA]
-									AND tc.[TABLE_NAME]=c.[TABLE_NAME]
-			                 WHERE kcu.TABLE_CATALOG=c.TABLE_CATALOG
-			                   AND kcu.TABLE_SCHEMA=c.TABLE_SCHEMA
-			                   AND kcu.TABLE_NAME=c.TABLE_NAME
-			                   AND kcu.COLUMN_NAME=c.COLUMN_NAME) IS_PK
-			FROM [INFORMATION_SCHEMA].[COLUMNS] c
-			order by 1,2,3,5
-			''') { row ->
-			
-			def t = new Table(table: row[2], schema: row[1], database: group, where: "-")
-			add t
+					def t = new Table(table: row[2], schema: row[1], database: group, where: "-")
+					add t
 
-			t = get(t.table, t.schema, t.database)
+					t = get(t.table, t.schema, t.database)
 
-			t.addColumn(new Column(column: row[3], dataType: row[6], isPrimaryKey: row[7] == null ? 0 : 1, tableRef: t, ordinal: row[4], testType: "-"))
-
-			}
-			loadModelFromFile()
+					t.addColumn(new Column(column: row[3], dataType: row[6], isPrimaryKey: row[7] == null ? 0 : 1, tableRef: t, ordinal: row[4], testType: "-"))
+				}
+		loadModelFromFile()
 	}
 
 	def loadModelFromFile() {
@@ -166,10 +230,10 @@ class Model {
 				t.addColumn(new Column(column: row[3], dataType: row[6], isPrimaryKey: row[7], tableRef: t, testType: "-", ordinal: row[4]))
 			}
 		}
-		
 
 
-		
+
+
 
 		def file = new File("${Variables.path}${Variables.model}")
 		if(file.exists()) {
